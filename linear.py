e1acb5a9dd3dbf2fbbac4b2fb770c5bfc13639a2
Rupatarini <tarini.rupeswari@gmail.com>
2024-08-21 19:55:00 +0530

    Create linear.py

---
+++ linear.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+california_housing = datasets.fetch_california_housing()
+df = pd.DataFrame(data=california_housing.data, columns=california_housing.feature_names)
+df['PRICE'] = california_housing.target
+print(df.head())
+X = df.drop('PRICE', axis=1)
+y = df['PRICE']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print("Mean Squared Error:", mse)
+print("R-squared:", r2)
